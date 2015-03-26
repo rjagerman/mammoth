feb7118c63587ab7900a1af4f6f1f112e46f0066
Rolf Jagerman <rjagerman@gmail.com>
2015-03-26 10:17:29 +0100

    Added basic scala setup

---
+++ src/main/scala/ch/ethz/inf/da/mammoth/Main.scala
@@ -0,0 +1,16 @@
+package ch.ethz.inf.da.mammoth
+
+/**
+ * The main class
+ */
+object Main {
+  
+  /**
+   * Main entry point of the application
+   */
+  def main(args: Array[String]) {
+    println("Hello world")
+  }
+
+}
+
